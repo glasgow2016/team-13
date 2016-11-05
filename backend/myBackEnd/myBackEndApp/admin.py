4582d10d6152cac884ba7735dce2a8de2a785162
ondrejbohdal <ondrej.bohdal@gmail.com>
2016-11-05 23:20:38 +0000

    overhaul of models#BestCodeEver#MakeCodeGreatAgain#GuyFawkesFantasticBugsAndWhereToFindThem

--- backend/myBackEnd/myBackEndApp/admin.py
+++ backend/myBackEnd/myBackEndApp/admin.py
@@ -1,17 +1,6 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-
-admin.site.register(StaffMember)
-admin.site.register(VisitType)
-admin.site.register(JourneyStage)
-admin.site.register(NatureOfVisit)
-admin.site.register(CancerSite)
-admin.site.register(Activity)
-admin.site.register(Activity_Record)
-admin.site.register(Gender)
-admin.site.register(Age)
 admin.site.register(Record)
-admin.site.register(Centre)
-admin.site.register(Person)
-admin.site.register(StaffGroup)
+admin.site.register(Activity)
+admin.site.register(StaffMember)
