839c34788072817c8ba32bec1ac5310b042a7e7e
ondrejbohdal <ondrej.bohdal@gmail.com>
2016-11-05 21:17:41 +0000

    saving of objects repaired database#BestCodeEver#MakeCodeGreatAgain

--- backend/myBackEnd/myBackEndApp/views.py
+++ backend/myBackEnd/myBackEndApp/views.py
@@ -5,8 +5,6 @@ from django.http import HttpResponse, JsonResponse
 from .models import *
 # Create your views here
 
-def index(request):
-	return render(request, "index.html")
 
 def index(request):
     return render(request, "index.html")
