fc2cbd7d592f316a85911ca6d5d5ec9ccd796cb8
vladbuzatu <buzatu.vlad@yahoo.com>
2016-11-05 20:27:39 +0000

    #GuyFawkes activities view added -  hope this works

--- backend/myBackEnd/myBackEndApp/urls.py
+++ backend/myBackEnd/myBackEndApp/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
 	
 	url(r'^$', views.index, name='index'),
-	url(r'login/$', views.login, name='login')
+	url(r'^login/$', views.login, name='login'),
+	url(r'^activities/$', views.activities, name='activities')
+	# url(r'^runscript/$', views.runscript, name='runscript')
 
 ]
--- backend/myBackEnd/myBackEndApp/views.py
+++ backend/myBackEnd/myBackEndApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from .models import *
+import json
 # Create your views here
 
 def index(request):
@@ -20,4 +21,24 @@ def login(request):
 	else:
 		return JsonResponse({"Status" : "Fail"})
 
+def activities(request):
+	activities = Activity.objects.all()
+	
+	activities_list = []
+	for activity in activities:
+		activities_list.append({"name": activity.name,
+								"category": activity.category,
+								"isCore": activity.isCore})
+				
+	return JsonResponse({"activities": activities_list})
 
+# def runscript(request):
+
+# 	gender = Gender.objects.first()
+
+# 	print gender.gender
+
+# 	record = Record(gender=gender)
+# 	record.save()
+
+# 	return HttpResponse("ok")
