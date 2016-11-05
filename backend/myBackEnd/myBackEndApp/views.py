081849a4fe8679b226e47a234d442bbfc9e0bc94
ondrejbohdal <ondrej.bohdal@gmail.com>
2016-11-05 23:32:15 +0000

    views save record#FantasticBugsAndWhereToFindThem#BestCodeEver#MakeCodeGreatAgain

--- backend/myBackEnd/myBackEndApp/views.py
+++ backend/myBackEnd/myBackEndApp/views.py
@@ -9,28 +9,40 @@ import json
 
 def index(request):
     return render(request, "index.html")
+
+
+def save_request(request):
+    # we just save the record without the activities for now
+    if request.method == 'POST':
+        record = Record(timeStamp=datetime.datetime.now(),
+                        gender=gender=request.body['gender'],
+                        age=request.body['age'],
+                        person=request.body['person'],
+                        visitType=request.body['visit_type'],
+                        journeyStage=request.body['journey_stage'],
+                        natureOfVisit=request.body['nature_of_visit'],
+                        cancerSite=request.body['cancer_site'])
+        record.save()
+
+
 def login(request):
-	userData = json.loads(request.body)
+    userData = json.loads(request.body)
+    username = userData["username"]
+    password = userData["password"]
+    user = StaffMember.objects.get(username=username)
+    if user.password == password:
+        return JsonResponse({"Status": "Success",
+                             "email": user.email})
+    else:
+        return JsonResponse({"Status": "Fail"})
 
-	username = userData["username"]
-	password = userData["password"]
-
-	user = StaffMember.objects.get(username = username)
-
-	if user.password == password:
-		return JsonResponse({"Status" : "Success",
-							 "email" : user.email})
-	else:
-		return JsonResponse({"Status" : "Fail"})
 
 def activities(request):
-	activities = Activity.objects.all()
-	
-	activities_list = []
-	for activity in activities:
-		activities_list.append({"name": activity.name,
-								"category": activity.category,
-								"isCore": activity.isCore})
-				
-	return JsonResponse({"activities": activities_list})
+    activities = Activity.objects.all()
+    activities_list = []
+    for activity in activities:
+        activities_list.append({"name": activity.name,
+                                "category": activity.category,
+                                "isCore": activity.isCore})
 
+    return JsonResponse({"activities": activities_list})
